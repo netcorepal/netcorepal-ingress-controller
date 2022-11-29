df9452fcc0d4f31aea72e07a9268be4ae71f5379
weiyu.xiao <weiyu.xiao@outlook.com>
2022-11-29 23:14:19 +0800

    add netcorepal config

--- src/NetCorePal.IngressController/Program.cs
+++ src/NetCorePal.IngressController/Program.cs
@@ -3,6 +3,12 @@ using Prometheus;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("/app/config/yarp.json", optional: true);
+
+var netcorepalConfig = builder.Configuration.GetSection("netcorepal");
+var useMetricServer = netcorepalConfig.GetValue<bool>("UseMetricServer", false);
+var useHttpMetrics = netcorepalConfig.GetValue<bool>("UseHttpMetrics", false);
+var useYarpProxyStateUI = netcorepalConfig.GetValue<bool>("UseYarpProxyStateUI", false);
+
 builder.Services.AddKubernetesReverseProxy(builder.Configuration);
 if (!builder.Environment.IsDevelopment())
 {
@@ -13,12 +19,23 @@ builder.Services.AddYarpProxyStateUI();
 var app = builder.Build();
 
 app.UseHealthChecks(path: "/healthz");
-app.UseMetricServer(url: "/metrics");
-app.UseYarpProxyStateUIStaticFiles();
-
+if (useMetricServer || useHttpMetrics)
+{
+    app.UseMetricServer(url: "/metrics");
+}
+if (useYarpProxyStateUI)
+{
+    app.UseYarpProxyStateUIStaticFiles();
+}
 app.UseRouting();
-app.UseHttpMetrics();
-app.UseYarpProxyStateUI(); // YARP Proxy State UI  http://yourip/_state
+if (useHttpMetrics)
+{
+    app.UseHttpMetrics();
+}
+if (useYarpProxyStateUI)
+{
+    app.UseYarpProxyStateUI(); // YARP Proxy State UI  http://yourip/_state
+}
 app.MapReverseProxy();
 
 app.Run();
